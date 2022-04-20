9aaa818548f9894058f6e30f0b5bd4655995966c
SikkiLadho <mushi.shar@gmail.com>
2022-04-21 03:24:16 +0500

    put all headers in includes.h

---
+++ includes.h
@@ -0,0 +1,15 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <ctype.h>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+
+#endif
